63c9e658ed4d0cb297bc9b0115f6948b64a2cdb6
abrahamor <abrahamortega99@gmail.com>
2023-03-29 19:13:04 -0600

    cambio de estado

--- components/Main.tsx
+++ components/Main.tsx
@@ -191,7 +191,7 @@ const Main = () => {
           {!loading && !errors && !token && <Text style={{color:'green'}}>{message}</Text>}
           {!loading && !errors && !token && <Text>Estado: bloqueado</Text>}
           {!loading && token   && <Text>Estado: desbloqueado</Text>}
-	  <UserCard nombre = {lock ? nombre  : userName} puesto={lock ? cargo  : puesto == 0 ? '' : puesto} correo={lock ?  correo : email} cel={lock ?  cel : telefono} date={lock ? fecha_stat : fecha == 'Fecha de nacimiento' ? '' : fecha} bg_photo={bg_photo} pf_photo={lock ? img : image}/> 
+	        <UserCard nombre = {lock ? nombre  : userName} puesto={lock ? cargo  : puesto == 0 ? '' : puesto} correo={lock ?  correo : email} cel={lock ?  cel : telefono} date={lock ? fecha_stat : fecha == 'Fecha de nacimiento' ? '' : fecha} bg_photo={bg_photo} pf_photo={lock ? img : image}/> 
         </View> 
       </ScrollView>
     </EmployeeContext.Provider>
